f8613196e650181f7cf2d07240c9f543fde957ad
ruslan2336 <81718051+ruslan2336@users.noreply.github.com>
2021-04-15 00:06:06 +0200

    Loop is added to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-let pokemonList = [{name: "Ruslan", types: ['grass', 'poison'], height: 100}, {name: "Anar", types: ['water', 'honey'], height: 20},{name: "Aida", types: ['grass', 'water'], height: 130}]
+let pokemonList = [
+	{
+		name: "Ruslan", 
+		types: ['grass', 'poison'], 
+		height: 100
+	}, 
+	{
+		name: "Anar", 
+		types: ['water', 'honey'], 
+		height: 20
+	},
+	{
+		name: "Aida", 
+		types: ['grass', 'water'], 
+		height: 130
+	}
+]
+for (let i = 1; i < personheight; i++) {
+  if (person[i].height <120 && person[i].height >50){
+    document.write(person[i].name + " is average");
+  }else if (person[i].height <50){
+    document.write(person[i].name + " is small");
+  }else {
+    document.write(person[i].name + " is tall");
+}
+let Personheight = [
+	{name: "Ruslan", height: 100},
+	{name: "Anar", height: 20},
+	{name: "Aida", height: 130}
+	];

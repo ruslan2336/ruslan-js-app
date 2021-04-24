34721e8044c3eafba34b7cc17fc1a553fe0c590c
ruslan2336 <81718051+ruslan2336@users.noreply.github.com>
2021-04-24 13:28:07 +0200

    IIFE code fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,3 @@
-let pokemonList = [
-	{
-		name: "Ruslan", 
-		types: ['grass', 'poison'], 
-		height: 100
-	}, 
-	{
-		name: "Anar", 
-		types: ['water', 'honey'], 
-		height: 20
-	},
-	{
-		name: "Aida", 
-		types: ['grass', 'water'], 
-		height: 130
-	}
-];
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height <120 && pokemonList[i].height >50){
-    document.write(pokemonList[i].name + " is average ");
-  }else if (pokemonList[i].height <50){
-    document.write(pokemonList[i].name + " is small ");
-  }else {
-    document.write(pokemonList[i].name + " is tall ");
-}
-}
-
 let pokemonRepository = (function () {
 	let pokemonList = [
 		{
@@ -46,9 +19,9 @@ let pokemonRepository = (function () {
 
   return {
     add: function(pokemon) {
-      pokemonList.push(pokemon);
+      pokemonList.add(item);
     },
-    function: getAll() {
+    function: pokemonList.getAll() {
       return pokemonList;
     }
   };
@@ -56,4 +29,4 @@ let pokemonRepository = (function () {
 
 console.log(pokemonRepository.getAll()); // []
 pokemonRepository.add({ name: 'Pikachu' });
-console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
+document.write(pokemon.name + '- Height: ' + pokemon.height + ', Type: ' + pokemon.type + '<br>');

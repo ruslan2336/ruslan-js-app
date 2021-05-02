efd6e9e695858ce920be0acde4fb1d71cf6bd6f7
ruslan2336 <81718051+ruslan2336@users.noreply.github.com>
2021-05-02 22:54:40 +0200

    Adding the list with buttons and event handling

--- js/scripts.js
+++ js/scripts.js
@@ -25,18 +25,31 @@ let pokemonRepository = (function () {
     function getAll() {
       return pokemonList;
     }
+	//adding function for the list with buttons
+	function addListItem(pokemonList) {
+	let elementAdd = document.querySelector('ul');
+	let listItem = document.createElement('li');
+	let button = document.createElement('button'); //adding the button
+	button.innerText = pokemonList.name;
+	button.classList.add('newbutton');
+	listItem.appendChild(button);
+	elementAdd.appendChild(listItem);
+	button.addEventListener('click', function (event) {
+	let target = event.target;
+	console.log(event);
+	});
+	}
+	function showDetails(pokemonList){
+	console.log()
+	}
 	return {
     add: add,
     getAll: getAll,
+	addListItem: addListItem
   }
 })();
 
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-	if (pokemon.height > 80) {
-document.write(`${pokemon.name}  (height: ${pokemon.height}) is adult<br>`);
-	}
-	else {
-		document.write(`${pokemon.name}  (height: ${pokemon.height}) is child<br>`)
-	}
+pokemonRepository.getAll().forEach(function (pokemonList) {
+	pokemonRepository.addListItem(pokemonList)
 });

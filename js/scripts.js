a314f22cc56b60583eb387db9aad273b81c35705
ruslan2336 <81718051+ruslan2336@users.noreply.github.com>
2021-05-27 23:20:06 +0200

    Code updated

--- js/scripts.js
+++ js/scripts.js
@@ -38,70 +38,54 @@ let pokemonRepository = (function () {
       console.error(e);
     });
   }
-	function showDetails(pokemon) {
-	loadDetails(pokemon).then(function () {
-      let modalContainer = document.querySelector('#modal-container');
-      function showModal(title, text) {
-        modalContainer.innerHTML = '';
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
+function showDetails(pokemon) {
+loadDetails(pokemon).then(function () {
+  let modalBody = document.querySelector(".modal-body");
+  let modalTitle = document.querySelector(".modal-title");
+  let modalHeader = document.querySelector(".modal-header");
 
-        let closeButtonElement = document.createElement('button');
-        closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'Close';
-        closeButtonElement.addEventListener('click', hideModal);
+  modalBody.innerHTML= "";
+  modalTitle.innerHTML = "";
+ // create h1 element for our pokemon name
+ let elementTitle = document.createElement("h1");
+  elementTitle.innerText= capitalize(pokemon.name);
+  modalTitle.appendChild(elementTitle);
 
-        let titleElement = document.createElement('h1');
-        titleElement.innerText = title;
+  //create p element for pokemon height and type
+ let elementHeightType = document.createElement("p");
+  elementHeightType.innerText =` is ${pokemon.height} meter(s) tall and is a ${returnValue(pokemon.types)}`;
+  modalBody.appendChild(elementHeightType);
+  // create image element for pokemon image
+  let imageElement = document.createElement("img");
+  modalBody.appendChild(imageElement);
+  imageElement.src = pokemon.imageUrl;
+  imageElement.classList.add("img-element")
+});
 
-        let contentElement = document.createElement('p');
-        contentElement.innerText = 'Height: ' + text;
-
-        modal.appendChild(closeButtonElement);
-        modal.appendChild(titleElement);
-        modal.appendChild(contentElement);
-        modalContainer.appendChild(modal);
-
-
-        modalContainer.classList.add('is-visible');
-      }
-
-      function hideModal() {
-        modalContainer.classList.remove('is-visible');
-      }
-
-      window.addEventListener('keydown', (e) => {
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-          hideModal();
-        }
-      });
-      modalContainer.addEventListener('click', (e) => {
-        // Since this is also triggered when clicking INSIDE the modal
-        // We only want to close if the user clicks directly on the overlay
-        let target = e.target;
-        if (target === modalContainer) {
-          hideModal();
-        }
-      });
-
-      document.querySelector('#show-modal').addEventListener('click', () => {
-        showModal(pokemon.name, pokemon.height);
-      });
-  });
 }
-		//adding function for the list with buttons
-	function addListItem(pokemonList) {
-	let elementAdd = document.querySelector('ul');
-	let listItem = document.createElement('li');
-	let button = document.createElement('button'); //adding the button
-	button.innerText = pokemonList.name;
-	button.classList.add('newbutton');
-	listItem.appendChild(button);
-	elementAdd.appendChild(listItem);
-	button.addEventListener('click', function (event) {
-	showDetails(pokemonList);
-	});
-	}
+function addListItem(pokemon){
+let listPokemon = document.querySelector(".pokemon-list");
+listPokemon.classList.add()
+let listItem = document.createElement("li");
+
+listItem.classList.add("list-group-item");
+listItem.classList.add("list-group-item-action");
+listItem.classList.add("mb-2")
+let button = document.createElement("button");
+button.innerText= pokemon.name;
+button.classList.add("btn");
+button.classList.add("btn-dark");
+button.classList.add("btn-block")
+button.setAttribute("data-target","#pokemonModal")
+button.setAttribute("data-toggle","modal")
+listPokemon.appendChild(listItem);
+listItem.appendChild(button);
+
+//add event listener to our items button that show details
+button.addEventListener("click",function(event){
+showDetails(pokemon)
+});
+}
   return {
     add: add,
     getAll: getAll,
